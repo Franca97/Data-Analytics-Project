6cf36e22fdae9b2da0a406dba0fa3ce54b863153
FrancaBoeglin <francaboeglin@Air-von-Franca.fritz.box>
2021-03-11 15:58:28 +0100

    Commit

---
+++ Swiss_Fertility.R
@@ -0,0 +1,19 @@
+# Packages
+install.packages("datasets.load")
+library(datasets.load)
+
+# Setting Working Directory 
+setwd("~/Library/Mobile Documents/com~apple~CloudDocs/Documents/GitHub/Data-Analytics-Project")
+
+# Dataset: Swiss 
+help(swiss)
+View(swiss)
+mydata <- swiss # Save dataset under the name "mydata"
+
+
+
+# Descriptive Statistics 
+
+
+
+# Simple Linear Regression 
