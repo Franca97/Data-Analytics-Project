9def37b05a3a803ebf04d6ee328edb3a3984d6ce
FrancaBoeglin <franca.boeglin@student.unisg.ch>
2021-03-25 09:54:19 +0100

    Update

--- Swiss_Fertility.R
+++ Swiss_Fertility.R
@@ -3,6 +3,7 @@
 library(datasets.load)
 library(tidyverse)
 library(ggplot2)
+library(dplyr)
 
 # Setting Working Directory 
 setwd("~/Library/Mobile Documents/com~apple~CloudDocs/Documents/GitHub/Data-Analytics-Project")
@@ -24,8 +25,7 @@ Rel.Freq_InfantMortality = mydata[,6]/length(mydata)
 
 #' Summary 
 summary(mydata)
-summary(mydata)
- 
+
 #' Boxplot
 boxplot(mydata)
 
@@ -35,9 +35,83 @@ hist(mydata$Fertility,main="Fertility",xlab="Fertility") # Fertility rates are m
 #' Density Plot of response variable 
 plot(density(mydata$Fertility), main = "Fertility") # a little bit right skewed 
 
-
 #'  Correlations 
-cor(mydata) # correlations with response variable lower than .8. No multicollinearity. 
+cor(mydata) # correlations with response variable lower than .8. 
 pairs(mydata) # assumption: linear relationship between education and examination/examination and agriculture 
 
- 
+
+## Exploring Data
+
+#' Top 10 Provinces with high fertility 
+mydata %>% 
+  select(Fertility) %>% 
+  arrange(desc(Fertility)) %>% 
+  head(10)
+
+#' Top 10 Provinces with low fertility 
+mydata %>% 
+  select(Fertility) %>% 
+  arrange(desc(Fertility)) %>% 
+  tail(10) # Cities: Geneve, Lausanne, Nyone 
+
+#' Relationship Agriculture Fertility
+mydata %>%  
+  ggplot() +
+  geom_point(mapping = aes(x = Agriculture, y = Fertility)) +
+  geom_smooth(mapping = aes(x = Agriculture, y = Fertility),
+              method = "lm") 
+
+#' Relationship Examination Fertility
+mydata %>%  
+  ggplot() +
+  geom_point(mapping = aes(x = Examination, y = Fertility)) +
+  geom_smooth(mapping = aes(x = Examination, y = Fertility),
+              method = "lm") 
+
+mydata %>%
+  ggplot() +
+  xlab("Examination") +
+  ylab("Fertility") +
+  geom_point(mapping = aes(
+    x = Examination,
+    y = Fertility,
+    color = Examination,
+    size = Fertility,
+    alpha = 0.5)) +
+  geom_smooth(mapping = aes(
+    x = Examination, 
+    y = Fertility),
+    method = "lm")
+
+#' Relationship Education Fertility
+mydata %>%  
+  ggplot() +
+  geom_point(mapping = aes(x = Education, y = Fertility)) +
+  geom_smooth(mapping = aes(x = Education, y = Fertility),
+              method = "lm") 
+
+#' Relationship Catholic Fertility
+mydata %>%  
+  ggplot() +
+  geom_point(mapping = aes(x = Catholic, y = Fertility)) +
+  geom_smooth(mapping = aes(x = Catholic, y = Fertility),
+              method = "lm") # Two regions as either high in catholic or low 
+
+#' Relationship Infant Mortality Fertility
+mydata %>%  
+  ggplot() +
+  geom_point(mapping = aes(x = Infant.Mortality, y = Fertility)) +
+  geom_smooth(mapping = aes(x = Infant.Mortality, y = Fertility),
+              method = "lm")
+
+
+### Empirical Analysis
+
+## Full Model 
+Reg_full <- lm(Fertility ~  Agriculture + Education + Examination + Catholic + Infant.Mortality, data = mydata)
+summary(Reg_full) # Examination not significant 
+
+Reg_fullwoexam <- lm(Fertility ~  Agriculture + Education + Catholic + Infant.Mortality, data = mydata)
+summary(Reg_fullwoexam)
+
+
